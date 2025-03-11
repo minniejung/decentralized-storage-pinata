a348e04778164c2493da0c5ae3f86a9cc6fa7d54
Insu-Lee <insu.lee@mrhi.or.kr>
2025-03-11 14:29:25 +0900

    fix: network setting

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -7,9 +7,9 @@ const config: HardhatUserConfig = {
   solidity: '0.8.28',
   networks: {
     kaia: {
-      url: 'https://public-en-kairos.node.kaia.io', 
+      url: 'https://public-en-kairos.node.kaia.io',
       accounts: [
-        process.env.PRIVATE_KEY || '', // Todo: Ganache에서 제공하는 프라이빗 키 사용(.env 파일을 사용합니다)
+        process.env.PRIVATE_KEY || '', // Todo: Kairos에서 제공하는 프라이빗 키 사용(.env 파일을 사용합니다)
       ],
     },
   },
